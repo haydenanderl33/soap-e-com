882d30031f68ddafceb354895eb801d13222988d
haydenanderl33 <hayden.anderl33@gmail.com>
2021-06-30 17:33:48 -0600

    fixing header margin

--- src/components/Landing/landing-elements.js
+++ src/components/Landing/landing-elements.js
@@ -3,13 +3,33 @@ import styled from "styled-components";
 export const LandingContainer1 = styled.div`
 height: 745px;
 width: 100%;
+
+`
+export const LandingImageBackground1 = styled.img`
+  position: absolute;
+  z-index: -3;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
 `
 export const LandingWrapper1 = styled.div`
 height: 100%;
 width: 100%;
+display: flex;
+flex-direction: column;
+position: relative;
+z-index: 0;
+
 `
-export const LandingImageBackground1 = styled.img`
-height: 100%;
-width: 100%;
+
+export const Landing1Row1 = styled.div`
+
+`
+export const Landing1Row2 = styled.div`
+
+`
+export const Landing1Row3 = styled.div`
 
 `
--- src/components/Landing/landing-index.js
+++ src/components/Landing/landing-index.js
@@ -1,17 +1,28 @@
-import React from 'react'
-import {LandingContainer1, LandingWrapper1, LandingImageBackground1} from './landing-elements'
+import React from "react";
+import {
+  LandingContainer1,
+  LandingWrapper1,
+  LandingImageBackground1,
+  Landing1Row1,
+  Landing1Row2,
+  Landing1Row3,
+} from "./landing-elements";
 
 const Landing = () => {
-    return (
-        <>
-            <LandingContainer1>
-                <LandingWrapper1>
-                    <LandingImageBackground1 src={process.env.PUBLIC_URL + '/images/LandingPageSoapBackground.jpg'}/>
+  return (
+    <>
+      <LandingContainer1>
+        <LandingImageBackground1
+          src={process.env.PUBLIC_URL + "/images/LandingPageSoapBackground.jpg"}
+        />
+        <LandingWrapper1>
+          <Landing1Row1>potato</Landing1Row1>
+          <Landing1Row2>potato</Landing1Row2>
+          <Landing1Row3>potato</Landing1Row3>
+        </LandingWrapper1>
+      </LandingContainer1>
+    </>
+  );
+};
 
-                </LandingWrapper1>
-            </LandingContainer1>
-        </>
-    )
-}
-
-export default Landing
+export default Landing;
