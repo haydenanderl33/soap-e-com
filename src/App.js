cb7a946bdee4abafbba2c84d42e31210aec1b058
haydenanderl33 <hayden.anderl33@gmail.com>
2021-06-30 15:52:24 -0600

    added dom and styled

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import firebase from './firebase';
 
 const App = () => {
 
-  console.log(firebase)
   return(
   <>
-  Checking hosting status
   
+
   </>)
 }
 export default App
