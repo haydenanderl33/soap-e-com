5329f08c35f7eea9b7dbdadf34c733f8e754aa95
haydenanderl33 <hayden.anderl33@gmail.com>
2021-07-06 17:54:53 -0600

    landing 1 done

--- src/components/Header/header-elements.js
+++ src/components/Header/header-elements.js
@@ -5,12 +5,17 @@ export const HeaderContainer = styled.div`
   width: 100%;
   position: fixed;
   background: #a1cdf4;
+  z-index: 99;
 `;
 export const HeaderWrapper = styled.div`
   height: 100%;
   width: 100%;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
+
+  @media screen and (max-width: 800px) {
+  display: none;
+  }
 `;
 export const HeaderColumn1 = styled.div`
   display: flex;
@@ -28,7 +33,7 @@ export const HeaderColumn2 = styled.div`
   justify-content: space-evenly;
   align-items: center;
   font-size: 48px;
-  font-family: 'Kaushan Script', cursive;
+  font-family: 'Cookie', cursive;
 `;
 export const HeaderColumn3 = styled.div`
   display: flex;
@@ -37,4 +42,30 @@ export const HeaderColumn3 = styled.div`
   justify-content: space-evenly;
   align-items: center;
   font-size: 24px;
+`;
+export const HeaderMobileWrapper = styled.div`
+    height: 100%;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+
+  @media screen and (min-width: 800px) {
+  display: none;
+  }
+`;
+export const MobileColumn1 = styled.div`
+font-size: 24px;
+color: #fff;
+display: flex;
+justify-content: flex-start;
+align-items:center;
+padding: 0 48px;
+`;
+export const MobileColumn2 = styled.div`
+font-size: 24px;
+color: #fff;
+display: flex;
+justify-content: flex-end;
+align-items:center;
+padding: 0 48px;
 `;
--- src/components/Landing/landing-elements.js
+++ src/components/Landing/landing-elements.js
@@ -1,35 +1,150 @@
 import styled from "styled-components";
+import { Link as LinkRoute } from "react-router-dom";
 
 export const LandingContainer1 = styled.div`
-height: 745px;
-width: 100%;
-
-`
-export const LandingImageBackground1 = styled.img`
+  height: 745px;
+  position: relative;
+`;
+export const LandingImageBackgroundWrapper1 = styled.div`
   position: absolute;
-  z-index: -3;
-  display: flex;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+  }
+`;
+export const LandingImageBackground1 = styled.img`
   width: 100%;
   height: 100%;
   -o-object-fit: cover;
   object-fit: cover;
-`
+  background: #7a82ab;
+`;
 export const LandingWrapper1 = styled.div`
-height: 100%;
-width: 100%;
-display: flex;
-flex-direction: column;
-position: relative;
-z-index: 0;
+  position: absolute;
+  z-index: 3;
+  height: 100%;
+  width: 100%;
+  display: grid;
+  padding-top: 128px;
+  padding-left: 12%;
+  grid-template-rows: repeat(4, 1fr);
+  color: #92dce5;
 
-`
+
+`;
 
 export const Landing1Row1 = styled.div`
+  width: 224px;
+  height: 100%;
+  font-size: 36px;
+  color: #fff;
+  font-weight: bold;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
 
-`
+  @media screen and (max-width: 800px) {
+    font-size: 24px;
+    width: 160px;
+  }
+`;
 export const Landing1Row2 = styled.div`
+  width: 224px;
+  height: 100%;
+  font-size: 24px;
+  color: #fff;
+  font-weight: bold;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
 
-`
-export const Landing1Row3 = styled.div`
-
-`
+  @media screen and (max-width: 800px) {
+    font-size: 24px;
+    width: 128px;
+  }
+`;
+export const Landing1Row3 = styled(LinkRoute)`
+height: 48px;
+width: 224px;
+color: #fff;
+background: #7C809B  ;
+font-size: 20px;
+text-decoration: none;
+display: flex;
+justify-content: center;
+align-items: center;
+border: 1px solid #fff;
+`;
+export const LandingContainer2 = styled.div`
+  height: 224px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  font-size: 24px;
+  @media screen and (max-width: 800px) {
+    grid-template-rows: repeat(5, 1fr);
+    grid-template-columns: none;
+  }
+`;
+export const Landing2Row1 = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Sriracha", cursive;
+  font-weight: bold;
+`;
+export const Landing2Row2 = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Bad Script", cursive;
+  font-weight: bold;
+`;
+export const Landing2Row3 = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Lateef", cursive;
+  font-weight: bold;
+`;
+export const Landing2Row4 = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Just Another Hand", cursive;
+  font-weight: bold;
+`;
+export const Landing2Row5 = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Mansalva", cursive;
+  font-weight: bold;
+`;
+export const LandingContainer3 = styled.div`
+  height: 512px;
+  width: 100%;
+`;
